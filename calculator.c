9022f2b995a1e5a5acb26c5364459d614382979b
Git2sep <66361624+Git2sep@users.noreply.github.com>
2020-10-08 13:30:06 +0530

    Update calculator.c

--- calculator.c
+++ calculator.c
@@ -5,20 +5,21 @@ int main()
 {
   int n1,n2;
   char operator;
-  printf("Enter an operator(+,-,/,*):");
+  printf("Enter an operator(+,-,/,*,^):");
   scanf("%c",&operator);
 
   printf("Enter two numbers:");
   scanf("%d%d",&n1,&n2);
   switch(operator) {
-    case'+' : printf("%d + %d =%d",n1,n2,n1+n2);
+    case'+' : printf("%d + %d =%d ",n1,n2,n1+n2);
               break;
-    case'-' : printf("%d - %d =%d",n1,n2,n1-n2);
+    case'-' : printf("%d - %d =%d ",n1,n2,n1-n2);
               break;
-    case'/' : printf("%d / %d =%d",n1,n2,n1/n2);
+    case'/' : printf("%d / %d =%d ",n1,n2,n1/n2);
               break;
-    case'*' : printf("%d * %d =%d",n1,n2,n1*n2);
+    case'*' : printf("%d * %d =%d ",n1,n2,n1*n2);
               break;
+    case'^' : printf("%d ^ %d = %d ",n1,n2,n1^n2);
 
     default: printf("Error!operator is wrong");
 
